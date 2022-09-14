00c91b586543e37a21ab2b5530ddf490adb95759
Doug Gallarda <github@mail.gallarda.net>
2022-09-14 10:08:39 -0500

    New Dockerfile for AWS Fargate

---
+++ aws/quota.js
@@ -0,0 +1,25 @@
+function checkQuota(r) {
+    if (!r.variables.counter) {
+        r.error("NEW COUNTER");
+        r.return(204);
+    } else if (r.variables.counter < r.variables.quota) {
+        r.error("QUOTA PASS");
+        r.return(204);
+    } else {
+        r.error("QUOTA EXCEEDED");
+        r.return(403);
+    }
+}
+function incrementQuota(r) {
+    if (!r.variables.counter) {
+        r.variables.counter = 1;
+        r.error("CREATING");
+    } else {
+        r.variables.counter++;
+        r.error("INCREMENTING");
+    }
+    r.error("COUNTER = " + r.variables.counter);
+    r.return(204);
+}
+
+export default {checkQuota, incrementQuota}
